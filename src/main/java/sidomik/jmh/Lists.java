497e7932750bb5d9e93fe6e82f158e35ea6c02b8
Mikhail Sidorkevich <michael.sidorkevich@gmail.com>
2019-08-23 23:38:31 +0300

    adding initial test size to ArrayList vs LinkedList JMH test

--- src/main/java/sidomik/jmh/Lists.java
+++ src/main/java/sidomik/jmh/Lists.java
@@ -1,17 +1,20 @@
 package sidomik.jmh;
 
-import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
-@Warmup(iterations = 20, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 40, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 50, time = 20, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 100, time = 10, timeUnit = TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Fork(4)
+@Fork(1)
 public class Lists {
 
     private List<String> list;
@@ -19,9 +22,15 @@ public class Lists {
     @Param({"java.util.ArrayList", "java.util.LinkedList"})
     private String listClassName;
 
+    @Param({"0", "1000", "1000000"})
+    private int initialListSize;
+
     @Setup(Level.Iteration)
     public void setup() throws Exception {
         list = (List<String>) Class.forName(listClassName).getDeclaredConstructor().newInstance();
+        for (int i = 0; i < initialListSize; i++) {
+            list.add("some test string");
+        }
     }
 
     @Benchmark
@@ -30,6 +39,11 @@ public class Lists {
     }
 
     public static void main(String[] args) throws Exception {
-        Main.main(args);
+        Options opt = new OptionsBuilder()
+                .jvmArgs("-Xms2g", "-Xmx2g", "-XX:+PrintFlagsFinal", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseEpsilonGC"
+                )
+                .addProfiler(GCProfiler.class)
+                .build();
+        new Runner(opt).run();
     }
 }
